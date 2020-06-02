3847ad47a9f00a3c0b1cf53441b185b90252975c
hqjs <hqjs.org@gmail.com>
2020-06-02 23:34:31 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # stream-buffer-cache
-Cache for node.js streams
+Cache for node.js streams. Stores stream results in a memory allowing to immediatly replay the stream.
+
+Speed up your services.
+
+Can be applied to any cache that implements `Map` interface e.g. regular `Map` or LRU-cache. It redefines `get` and `set` methods to make a `Map` works with streams.
+
+Usefull for caching slow repetitive streaming connections, such as S3 requests or complex database queries.
+
+
+Installation
+------------
+
+```npm i @hqjs/stream-buffer-cache```
+
+
+Example
+-------------
+
+```js
+// You can pass any class that implements Map interface
+const LRUMap = require('lru-cache');
+const Cache = require('@hqjs/stream-buffer-cache')(LRUMap);
+
+// LRU cache options
+const options = {
+  max: 500,
+  maxAge: 1000 * 60 * 60,
+};
+
+// Pass your Map class option to Cache constructor
+const cache = new Cache(options);
+const fs = require('fs');
+
+fs.createReadStream('./buffer-cache.js', { encoding: 'utf8' })
+  .pipe(cache.set('test'));
+
+cache.get('test')
+  .pipe(fs.createWriteStream('./buffer-cache.js.test'));
+```
+
+
+API
+---
+
+This methods from parent `Map` class are overriden
+
+##### set(key, [bytes = 0])
+returns a Duplex stream
+```js
+fileStream.pipe(cache.set('key')).pipe(res);
+```
+`bytes` is optional parameter that hints to preallocate buffer for cache
+
+
+##### get(key) => ReadableStream
+
+```js
+const cached = cache.get('key');
+if (cached) {
+	cached.pipe(res);
+}
+```
---
+++ index.js
@@ -0,0 +1,102 @@
+const { Duplex, Readable } = require('stream');
+
+const createReadMethod = (entry, state) => function() {
+  if (state.alreadyRead < entry.available) {
+    const { readableHighWaterMark } = this;
+    const chunkSize = Math.min(readableHighWaterMark, entry.available - state.alreadyRead);
+    const chunk = entry.buffer.slice(state.alreadyRead, state.alreadyRead + chunkSize);
+    this.push(Buffer.from(chunk));
+    state.alreadyRead += chunkSize;
+    if (state.readAttempts > 0) state.readAttempts--;
+  } else if (entry.complete) {
+    this.push(null);
+  } else {
+    state.readAttempts++;
+  }
+};
+
+const streamBufferCache = Cls => class extends Cls {
+  set(key, bytes = 0) {
+    const state = {
+      alreadyRead: 0,
+      readAttempts: 0,
+    };
+
+    const entry = {
+      available: 0,
+      buffer: new Array(bytes),
+      complete: false,
+      stream: new Duplex({
+        write(chunk, encoding, cb) {
+          const { byteLength } = chunk;
+          for (const b of chunk) entry.buffer.push(b);
+          entry.available += byteLength;
+          if (state.readAttempts > 0) {
+            entry.stream.push(chunk);
+            state.alreadyRead += byteLength;
+            state.readAttempts--;
+          }
+          return cb();
+        },
+      }),
+    };
+
+    entry.stream['_read'] = createReadMethod(entry, state);
+
+    entry.stream.on('error', () => {
+      this.delete(key);
+      process.nextTick(() => entry.stream.removeAllListeners());
+    });
+
+    entry.stream.once('finish', () => {
+      if (state.readAttempts > 0) entry.stream.push(null);
+      entry.complete = true;
+    });
+
+    super.set(key, entry);
+
+    return entry.stream;
+  }
+
+  get(key) {
+    const entry = super.get(key);
+    if (!entry) return undefined;
+
+    const state = {
+      alreadyRead: 0,
+      readAttempts: 0,
+    };
+
+    const stream = new Readable;
+
+    stream['_read'] = createReadMethod(entry, state);
+
+    if (!entry.complete) {
+      const onData = chunk => {
+        if (state.readAttempts > 0) {
+          stream.push(chunk);
+          state.alreadyRead += chunk.byteLength;
+          state.readAttempts--;
+        }
+      };
+      const onError = err => {
+        stream.emit('error', err);
+      };
+
+      entry.stream.on('data', onData);
+
+      entry.stream.on('error', onError);
+
+      entry.stream.once('finish', () => {
+        if (state.readAttempts > 0) stream.push(null);
+
+        entry.stream.off('data', onData);
+        entry.stream.off('error', onError);
+      });
+    }
+
+    return stream;
+  }
+};
+
+module.exports = streamBufferCache;
